b23da8086fe5370e10403119985867e464089f6b
Manish Ghimire <manishghimire@hotmail.com>
2025-09-05 09:47:35 +1000

    Demo to Aslam

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="app-container">
-      <h1 className="main-title">Restaurant Booking App - Customer view</h1>
+      <h1 className="main-title">Restaurant Booking Application - Customer view</h1>
     </div>
   )
 }

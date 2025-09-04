67fde8de6f45041955d58e1d88385c713bf32e48
Manish Ghimire <manishghimire@hotmail.com>
2025-09-04 23:16:49 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="main-title">Restaurant Booking App - Customer view</h1>
+    </div>
+  )
+}
+
+export default App

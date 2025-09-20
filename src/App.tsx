12d774be7ccdda79a4dd51ff0b0c543c82a62f65
Manish Ghimire <manishghimire@hotmail.com>
2025-09-20 23:10:34 +1000

    Super admin link removed from topbar / unused yml file removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import SuperAdmin from "./SuperAdmin";
 import RestaurantOwnerDashboardPage from "./features/restaurantOwner/pages/RestaurantOwnerDashboard";
 import CustomerDashboard from "./features/customer/pages/CustomerDashboard";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <nav className="p-4 border-b border-gray-200 mb-6 flex justify-between bg-white shadow-sm">
+      <nav className="p-2 border-b border-gray-200 mb-6 flex gap-4 bg-white shadow-sm">
         <Link
-          to="/customer"
-          className="mr-4 text-blue-600 hover:text-blue-800 font-medium"
+          to="/"
+          className=" text-blue-600 hover:text-blue-800 font-medium"
         >
           Customer
         </Link>
@@ -20,21 +19,14 @@ const App: React.FC = () => {
         >
           Restaurant Owner
         </Link>
-        <Link
-          to="/superAdmin"
-          className="text-blue-600 hover:text-blue-800 font-medium"
-        >
-          Super Admin
-        </Link>
+
       </nav>
       <Routes>
-        <Route path="/customer/*" element={<CustomerDashboard />} />
-
+        <Route path="/" element={<CustomerDashboard />} />
         <Route
           path="/restaurantOwner/*"
           element={<RestaurantOwnerDashboardPage />}
         />
-        <Route path="/superAdmin" element={<SuperAdmin />} />
       </Routes>
     </Router>
   );

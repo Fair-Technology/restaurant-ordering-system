e9b040c37e1629e7841172cb2dfcbb5fbea8cace
Aslam <dakaevas13@gmail.com>
2025-09-24 18:52:10 +1000

    login and logout buttons

--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -1,7 +1,7 @@
 export const msalConfig = {
   auth: {
-    clientId: "f874abc6-2109-47f1-85ff-4531b83fbf7f", // Application (client) ID
-    authority: "https://fairtechnology.ciamlogin.com/a48107d6-1e60-4988-8093-ba493a00bb55", // Directory (tenant) ID
+    clientId: "5940f49f-d6ec-4865-b38e-379de583765c", // Application (client) ID
+    authority: "https://fairaustraliaextusers.ciamlogin.com/d220f4ca-2ba2-436a-a437-5779ae23584d", // https://<your-tenant-name>.ciamlogin.com/<Directory (tenant) ID>
     redirectUri: "http://localhost:5173", // or your deployed URL
   }
 };
--- src/pages/superAdmin/login.tsx
+++ src/pages/superAdmin/login.tsx
@@ -1,21 +1,31 @@
-
 import { useMsal } from "@azure/msal-react";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 
 const SuperAdmin = () => {
-  const { instance } = useMsal();
-   const navigate = useNavigate();
+  const { instance, accounts } = useMsal();
+  //const navigate = useNavigate();
 
   const handleLogin = () => {
     instance.loginRedirect();
-     //navigate("/superAdminDashboard"); 
+    //navigate("/superAdminDashboard"); 
   };
+
+  const handleLogout = () => {
+    instance.logoutRedirect();
+  };
+
   return (
     <div style={{ textAlign: 'center', marginTop: '2rem' }}>
       <h2>Admin</h2>
-      <button onClick={handleLogin} className="bg-red-300 text-green-500">
-        Login
-      </button>
+      {!accounts.length ? (
+        <button onClick={handleLogin} className="bg-red-300 text-green-500">
+          Login
+        </button>
+      ) : (
+        <button onClick={handleLogout} className="bg-blue-300 text-red-500">
+          Logout
+        </button>
+      )}
     </div>
   );
 };
